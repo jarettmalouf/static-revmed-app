46bc25f56ab1be94ccca661e799f86a1c7e130f0
Jarett Malouf <jarett.malouf@credera.com>
2021-06-24 16:02:36 +0200

    change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  return <div>Hllo {value}</div>;
 }
 
 export default App;
